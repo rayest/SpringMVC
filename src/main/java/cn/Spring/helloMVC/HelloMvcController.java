30e296f718f06ea56768c8460d9c73cdcfb85ad4
bogehu <bogehu@hucongzheng.cn>
2016-06-07 00:07:19 +0800

    test Commit and Push

--- src/main/java/cn/Spring/helloMVC/HelloMvcController.java
+++ src/main/java/cn/Spring/helloMVC/HelloMvcController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloMvcController {
     @RequestMapping("/mvc")
     public String helloMvc(){
-        return "home";
+        return  "home";
     }
 }
