8df75bd4f37788693e9b6c4514ef4f753c72bf22
bogehu <bogehu@hucongzheng.cn>
2016-06-12 23:31:58 +0800

    SpringMVCDemo successfully done.

--- src/main/java/cn/Spring/helloMVC/HelloMvcController.java
+++ src/main/java/cn/Spring/helloMVC/HelloMvcController.java
@@ -1,16 +1,27 @@
 package cn.Spring.helloMVC;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by bogehu on 16/5/24.
  */
 @Controller
 @RequestMapping("/hello")
-public class HelloMvcController {
+public class HelloMVCController {
     @RequestMapping("/mvc")
-    public String helloMvc(){
+    public String helloMVC(){
         return  "home";
     }
+    @RequestMapping("/rest")
+    @ResponseBody
+    public JSONObject helloRest(){
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("name","bogehu");
+        return jsonObject;
+    }
+
 }
