5166a1585d567474252c6fb95931d03046db0d11
Rayest <yi@rayest.cn>
2016-08-26 08:31:25 +0800

    基于 Java 配置的 spring 的例子

---
+++ src/main/java/cn/candy/annotation/annotationBean/FunctionService.java
@@ -0,0 +1,10 @@
+package cn.candy.annotation.annotationBean;
+
+/**
+ * Created by Rayest on 2016/8/26 0026.
+ */
+public class FunctionService {
+    public String sayHello(String word){
+        return "Hello " + word + " !";
+    }
+}
---
+++ src/main/java/cn/candy/annotation/annotationBean/UseFunctionService.java
@@ -0,0 +1,16 @@
+package cn.candy.annotation.annotationBean;
+
+/**
+ * Created by Rayest on 2016/8/26 0026.
+ */
+public class UseFunctionService {
+    FunctionService functionService;
+
+    public void setFunctionService(FunctionService functionService) {
+        this.functionService = functionService;
+    }
+
+    public String sayHello(String word){
+        return functionService.sayHello(word);
+    }
+}
