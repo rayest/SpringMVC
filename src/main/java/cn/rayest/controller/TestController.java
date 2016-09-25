cb8230352b47d9fa10b184ec912d24b516056cc7
Rayest <yi@rayest.cn>
2016-09-26 00:03:35 +0800

    springMVC 数据绑定：简单对象和多级对象的数据的获取处理

--- src/main/java/cn/rayest/controller/TestController.java
+++ src/main/java/cn/rayest/controller/TestController.java
@@ -1,5 +1,7 @@
 package cn.rayest.controller;
 
+import cn.rayest.model.Book;
+import cn.rayest.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,14 +18,14 @@ public class TestController {
     // 基本类型的数据，其值不能为空: 必须有 age 参数值
     @RequestMapping(value = "/baseType/age", method = RequestMethod.GET)
     @ResponseBody
-    public String baseType(int age){
+    public String baseType(int age) {
         return "name:" + age;
     }
 
     // @RequestParam:要求该注解的参数必须传
     @RequestMapping(value = "/baseType/name", method = RequestMethod.GET)
     @ResponseBody
-    public String baseType2(@RequestParam (value = "name") String name){
+    public String baseType2(@RequestParam(value = "name") String name) {
         return "name:" + name;
     }
 
@@ -31,7 +33,7 @@ public class TestController {
     // springMVC 对包装类型的数据处理, 传入的数据可以为空
     @RequestMapping(value = "/boxType/age", method = RequestMethod.POST)
     @ResponseBody
-    public String boxType(Integer age){
+    public String boxType(Integer age) {
         return "age:" + age;
     }
 
@@ -40,12 +42,27 @@ public class TestController {
     // 数组类型的数据的绑定
     @RequestMapping(value = "/array", method = RequestMethod.GET)
     @ResponseBody
-    public String getArray(String[] names){
+    public String getArray(String[] names) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (String name : names){
+        for (String name : names) {
             stringBuilder.append(name).append(", ");
         }
         return stringBuilder.toString();
     }
 
+    // 简单对象的数据绑定
+    // TODO:  http://localhost:8080/book?name=sanpao&author=sipao
+    @RequestMapping(value = "/book", method = RequestMethod.GET)
+    @ResponseBody
+    public String getBook(Book book) {
+        return book.toString();
+    }
+
+    // 多层级对象的数据绑定
+    // TODO  http://localhost:8080/user/address?id=123&userName=lee&password=456&address.country=china&address.city=nanjing&address.code=210094
+    @RequestMapping(value = "/user/address", method = RequestMethod.POST)
+    @ResponseBody
+    public String getUser(User user){
+        return user.toString();
+    }
 }
