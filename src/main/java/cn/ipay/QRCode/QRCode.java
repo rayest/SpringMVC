426107728a98499b05a07e3130fa11abefcb37d3
Rayest <yi@rayest.cn>
2016-10-03 20:02:38 +0800

    读取二维码的内容

--- src/main/java/cn/ipay/QRCode/QRCode.java
+++ src/main/java/cn/ipay/QRCode/QRCode.java
@@ -1,24 +1,28 @@
 package cn.ipay.QRCode;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 
 /**
  * Created by Rayest on 2016/10/3 0003.
- * 添加 jar 包：  F:\Rayest\jar包
+ * 1. 添加 jar 包：  F:\Rayest\jar包
+ * 2. 编码
  */
 
 // 生成二维码
 public class QRCode {
-    public void createQRCode(){
+    public void createQRCode() {
         int width = 300; // 二维码图片的宽度
         int height = 300; // 二维码图片的高度
         String format = "png"; // 二维码的格式
@@ -32,11 +36,33 @@ public class QRCode {
 
         // 生成二维码
         try {
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE,width, height);
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height);
             Path file = new File("F:/code/zxing.png").toPath(); // 生成二维码的保存路径
             MatrixToImageWriter.writeToPath(bitMatrix, format, file);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Result readQRCode() {
+        MultiFormatReader formatReader = new MultiFormatReader();
+        File file = new File("F:/code/zxing.png");
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
+        HashMap hints = new HashMap();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        Result result = null;
+        try {
+            result = formatReader.decode(binaryBitmap, hints);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/test/java/cn/ipay/QRCode/QRCodeTest.java
+++ src/test/java/cn/ipay/QRCode/QRCodeTest.java
@@ -1,7 +1,10 @@
 package cn.ipay.QRCode;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -15,5 +18,17 @@ public class QRCodeTest {
         QRCode qrCode = new QRCode();
         qrCode.createQRCode();
         assertNotNull(qrCode);
+    }
+
+    @Test
+    public void testReadQRCode() throws Exception {
+        QRCode qrCode = new QRCode();
+        Result result = qrCode.readQRCode();
+        String resultString = result.toString();
+        BarcodeFormat barcodeFormat = result.getBarcodeFormat();
+        String content = result.getText();
+        assertEquals("https://github.com/rayest", resultString);
+        assertEquals(BarcodeFormat.QR_CODE, barcodeFormat);
+        assertEquals("https://github.com/rayest", content);
     }
 }
